8cbee3687212c3da9220c6553b1c7d9ba42b8824
root <root@MSI.localdomain>
2021-11-17 17:20:04 +0700

    Jawaban UTS

---
+++ UTS/soal4.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void cetakUmur(int *umur){
+printf("Berarti Usia Kalian sekarang adalah  %d tahun", *umur);
+}
+int main(){
+int angka=21;
+cetakUmur(&angka);
+getchar();
+return 0;
+}
